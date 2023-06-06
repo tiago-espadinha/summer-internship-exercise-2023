6f99f7460b41708d74ae42b80c406eddde6df597
espadela <tiago.espadinha.simoes@gmail.com>
2023-06-06 03:06:08 +0100

    Implement method + few tests
    Implemented getSnailShell
    Added some simple tests

--- src/main/java/com/premiumminds/internship/snail/SnailShellPattern.java
+++ src/main/java/com/premiumminds/internship/snail/SnailShellPattern.java
@@ -1,5 +1,6 @@
 package com.premiumminds.internship.snail;
 
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Future;
 
 /**
@@ -14,6 +15,55 @@ class SnailShellPattern implements ISnailShellPattern {
    * @return order array of values thar represent a snail shell pattern
    */
   public Future<int[]> getSnailShell(int[][] matrix) {
-    throw new RuntimeException("Not Implemented Yet");
+
+    CompletableFuture<int[]> future = new CompletableFuture<>();
+    
+    
+    // If the matrix is empty, return an empty array
+    if (matrix.length == 1 && matrix[0].length == 0){
+      future.complete(new int[]{});
+      return future;
+    }
+    
+    int[] result = new int[matrix.length * matrix.length];
+    
+    // Initial boundries of the matrix: outer most layers to add to the result array
+    int left_bound = 0, top_bound = 0, bottom_bound = matrix.length - 1, right_bound = matrix.length - 1;
+    int result_index = 0;
+
+    // Add the values to the result array while the bounds do not cross each other
+    while(left_bound <= right_bound && top_bound <= bottom_bound){
+      // Add the values of the top row inside the vertical bounds (from left to right)
+      for (int i = left_bound; i <= right_bound; i++){
+        result[result_index] = matrix[top_bound][i];
+        result_index++;
+      }
+      top_bound++; // Update the top bound
+
+      // Add the values of the right column inside the horizontal bounds (from top to bottom)
+      for (int i = top_bound; i <= bottom_bound; i++){
+        result[result_index] = matrix[i][right_bound];
+        result_index++;
+      }
+      right_bound--; // Update the right bound
+
+      // Add the values of the bottom row inside the vertical bounds (from right to left)
+      for (int i = right_bound; i >= left_bound; i--){
+        result[result_index] = matrix[bottom_bound][i];
+        result_index++;
+      }
+      bottom_bound--; // Update the bottom bound
+
+      // Add the values of the left column inside the horizontal bounds (from bottom to top)
+      for (int i = bottom_bound; i >= top_bound; i--){
+        result[result_index] = matrix[i][left_bound];
+        result_index++;
+      }
+      left_bound++; // Update the left bound
+
+    }
+
+    future.complete(result);
+    return future;
   };
 }
--- src/test/java/com/premiumminds/internship/snail/SnailShellPatternTest.java
+++ src/test/java/com/premiumminds/internship/snail/SnailShellPatternTest.java
@@ -34,4 +34,52 @@ public class SnailShellPatternTest {
     int[] expected = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
     assertArrayEquals(result, expected);
   }
+
+
+  @Test
+  public void SnailShellPatternTestSize0Test()
+      throws InterruptedException, ExecutionException, TimeoutException {
+    int[][] matrix = {{}};
+    Future<int[]> count = new SnailShellPattern().getSnailShell(matrix);
+    int[] result = count.get(10, TimeUnit.SECONDS);
+    int[] expected = {};
+    assertArrayEquals(result, expected);
+  }
+
+
+  @Test
+  public void SnailShellPatternTestSize1Test()
+      throws InterruptedException, ExecutionException, TimeoutException {
+    int[][] matrix = { {1} };
+    Future<int[]> count = new SnailShellPattern().getSnailShell(matrix);
+    int[] result = count.get(10, TimeUnit.SECONDS);
+    int[] expected = { 1 };
+    assertArrayEquals(result, expected);
+  }
+
+
+  @Test
+  public void SnailShellPatternTestSize2Test()
+      throws InterruptedException, ExecutionException, TimeoutException {
+    int[][] matrix = { { 1, 2}, 
+                       { 4, 3} };
+    Future<int[]> count = new SnailShellPattern().getSnailShell(matrix);
+    int[] result = count.get(10, TimeUnit.SECONDS);
+    int[] expected = { 1, 2, 3, 4 };
+    assertArrayEquals(result, expected);
+  }
+
+
+  @Test
+  public void SnailShellPatternTestSize4Test()
+      throws InterruptedException, ExecutionException, TimeoutException {
+    int[][] matrix = { {  1,  2,  3, 4}, 
+                       { 12, 13, 14, 5}, 
+                       { 11, 16, 15, 6},
+                       { 10,  9,  8, 7} };
+    Future<int[]> count = new SnailShellPattern().getSnailShell(matrix);
+    int[] result = count.get(10, TimeUnit.SECONDS);
+    int[] expected = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 };
+    assertArrayEquals(result, expected);
+  }
 }
